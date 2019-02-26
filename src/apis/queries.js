8102a619b1331a4f56e956d259498dcf98ca641e
Adam Gedney <adam@olympussat.com>
2019-02-26 16:14:25 -0500

    WORKIG on queries file

--- src/apis/queries.js
+++ src/apis/queries.js
@@ -1,5 +1,99 @@
+import createHistory from 'history/createBrowserHistory';
+import createMemoryHistory from 'history/createMemoryHistory';
+// import _ from 'underscore';
+
 export default class{
   constructor(rxdux, options, instance) {
+    /** 
+     * Memory History is used by the test runner as there's no DOM. 
+     * createHistory requires a DOM 
+     * */
+    try{ this.history = createHistory(); }
+    catch(e){ this.history = createMemoryHistory(); }
+  }
 
+  /**
+   * Makes the query object to be translated according to output type specification
+   * @param filters
+   * @returns {*}
+   * @private
+   */
+  _makeQueryObject(filters) {
+    return filters
+      .map(filter => (
+        { [filter.id]: filter.range 
+          ? filter.range 
+          : filter.value 
+        })
+      )
+      .reduce((sum, query) => {
+        const key = Object.keys(query)[0];
+
+        // Check if the property to filter on exists already;
+        // Then check if it's an array.
+        // make it an array and push the value
+        if (sum.hasOwnProperty(key) && !(key.indexOf('sort-') > -1)) {
+          if (Array.isArray(sum[key])) {
+            sum[key].push(query[key]);// Add the new value
+          } else {
+            sum[key] = [sum[key]];// Extract the string value and transform to an array
+            sum[key].push(query[key]);//Add the new value
+          }
+        } else {
+          sum[key] = query[key];// First run, add the string
+        }
+        // return the mutated object
+        return sum;
+      }, {});
+  }
+
+  /**
+   * Returns the query parameter based on state.filters
+   * @param queryObject
+   */
+  _makeQueryString(queryObject) {
+
+    // Remove anything with a null value
+    // Handles single values and range values
+    Object.keys(queryObject)
+      .forEach((key) => (queryObject[key] == null
+        || ((queryObject[key].hasOwnProperty('start') && queryObject[key].hasOwnProperty('end'))
+          && (queryObject[key].start == null && queryObject[key].end == null)))
+        && delete queryObject[key]);
+
+      let queryString = 
+      Object.keys(queryObject)
+        .map(k => {
+          let attribute = k,
+            value = queryObject[k],
+            tmp_query = '';
+
+          if (value.hasOwnProperty('start') && value.hasOwnProperty('end')) {
+            tmp_query = `${attribute}--start=${value.start}&${attribute}--end=${value.end}`;
+          } else {
+            tmp_query = `${attribute}=${value}`;
+          }
+
+          return tmp_query;
+        })
+        .reduce((sum, query) => {
+
+          //Handle missing &
+          if (query.charAt(0) !== '&') {
+            query = '&' + query;
+          }
+
+          // Combine query strings
+          return sum + query;
+        }, '');
+
+    // Handle the ? character at the beginning of the string
+    if (queryString.charAt(0) === '&') {
+      queryString = queryString.replace(/^&/, '?');
+    } else if (queryString.charAt(0) !== '&' || queryString.charAt(0) !== '?') {
+      queryString = '?' + queryString;
+    }
+
+    return queryString;
   }
 }
--- test/specs/apis/queries.spec.js
+++ test/specs/apis/queries.spec.js
@@ -1,14 +1,42 @@
-import { expect } from 'chai';
+import { expect, assert } from 'chai';
 import QueriesApi from 'Src/apis/queries.js';
 import optionsExample from 'Src/options.example.js';
 
 describe('The Queries API ', () => {
   let queriesApi;
+  let queryObject = {
+    state: ["cbd0a696-2b4f-4469-85d1-f7027345e3e0"],
+    genres: ["87fc3814-4cb9-43a5-b723-63ecebd65c5a", "cdc3d520-8b74-46ac-9f4c-8f27d04ab49f"],
+    languages: ["9d5741e1-b482-4027-8c57-45193073ef12"],
+    entityType: ["olyplat-entity-movie"]
+  };
+  let queryString = '?state=cbd0a696-2b4f-4469-85d1-f7027345e3e0&genres=87fc3814-4cb9-43a5-b723-63ecebd65c5a,cdc3d520-8b74-46ac-9f4c-8f27d04ab49f&languages=9d5741e1-b482-4027-8c57-45193073ef12&entityType=olyplat-entity-movie';
+  let filters = [
+    {"id":"state","type":"select","prop":"state","label":"State","value":["cbd0a696-2b4f-4469-85d1-f7027345e3e0"],"multiple":true,"options":{"key":"entityUUID","value":"entityValue"}},
+    {"id":"genres","type":"select","prop":"genres","label":"Genres","value":["87fc3814-4cb9-43a5-b723-63ecebd65c5a","cdc3d520-8b74-46ac-9f4c-8f27d04ab49f"],"multiple":true,"multi":true,"options":{"key":"entityUUID","value":"entityValue"}},
+    {"id":"languages","type":"select","prop":"languages","label":"Languages","value":["9d5741e1-b482-4027-8c57-45193073ef12"],"multiple":true,"options":{"key":"entityUUID","value":"entityValue"}},
+    {"id":"entityType","type":"select","prop":"entityType","label":"Show Type","value":["olyplat-entity-movie"],"multiple":true,"options":{"key":"entityUUID","value":"entityValue"}}
+  ];
+
   beforeEach(function() {
     queriesApi = new QueriesApi(optionsExample);
   });
 
-	it('should instantiate', () => {
-    expect(queriesApi).to.be.instanceOf(QueriesApi);
-  });
+  it('should instantiate', () => expect(queriesApi).to.be.instanceOf(QueriesApi));
+  it('should have a history instance', () => assert.ok(queriesApi.history));
+
+  it('should have [_makeQueryObject] method', () => assert.typeOf(queriesApi._makeQueryObject, 'function'));
+  it('should have [_makeQueryString] method', () => assert.typeOf(queriesApi._makeQueryString, 'function'));
+  it('should have [_writeQueryStringToURL] method', () => assert.typeOf(queriesApi._writeQueryStringToURL, 'function'));
+  // it('should have [getQueryObject] method', () => assert.typeOf(queriesApi.getQueryObject, 'function'));
+  // it('should have [getQueryString] method', () => assert.typeOf(queriesApi.getQueryString, 'function'));
+  // it('should have [getFullUrl] method', () => assert.typeOf(queriesApi.getFullUrl, 'function'));
+  // it('should have [removeQueryStringFromUrl] method', () => assert.typeOf(queriesApi.removeQueryStringFromUrl, 'function'));
+
+  it('_makeQueryObject should convert a filters collection to a query object', () =>
+    expect(queriesApi._makeQueryObject(filters)).to.eql(queryObject));
+
+  it('_makeQueryString should convert a query to a query string', () => 
+    expect(queriesApi._makeQueryString(queryObject)).to.eql(queryString));
+
 });
