012640dfc80905965b9f6b2e3d9a7ac5ca1b6980
Adam Gedney <adam@olympussat.com>
2019-02-27 12:54:42 -0500

    WORKING on query string tests

--- src/apis/queries.js
+++ src/apis/queries.js
@@ -20,12 +20,7 @@ export default class{
    */
   _makeQueryObject(filters) {
     return filters
-      .map(filter => (
-        { [filter.id]: filter.range 
-          ? filter.range 
-          : filter.value 
-        })
-      )
+      .map(filter => ({ [filter.id]: filter.range || filter.value }))
       .reduce((sum, query) => {
         const key = Object.keys(query)[0];
 
@@ -95,5 +90,121 @@ export default class{
     }
 
     return queryString;
+  }
+
+  /**
+ * Writes the query string to the url
+ * @param queryString
+ * @param options
+ * @private
+ */
+  _writeQueryStringToURL(queryString, options) {
+    // @todo add url_encode and base^4 encode and decode to the write system
+    // Only allow if the config file specifies
+    if (options.writeQueryStringToURL && (queryString || queryString === null)) {
+      // const path = this.history.location.href.split('?')[0].split(this.history.location.host)[1];
+      const path = this.history.location.search;
+
+      if (queryString === null) { queryString = ''; }
+
+      let replaceURL = (path + queryString + '&' + this._getPaginationQueryParams()).replace(/&+$/, "");
+
+      if (options.clearPaginationQueryString) {
+        replaceURL = path + queryString;
+      }
+
+      this.history.replace(replaceURL);
+
+      return this.history;
+    }
+  }
+
+  /**
+   * Gets the pagination query params from the url to preserve them on write
+   */
+  _getPaginationQueryParams() {
+    // const params = _.pick(this.parseParms(this.readQueryStringFromURL()), ['skip', 'take', 'page']);
+    const params = Object.entries(this._parseParams(this._readQueryStringFromURL()))
+      .reduce((acc, [k, v]) => {
+        if (['skip', 'take', 'page'].includes(k)) {
+          acc[k] = v;
+        }
+        return acc;
+      }, {});
+    let str = '';
+
+    // @todo convert this to a reducer
+    for (let key in params) {
+      str += `${key}=${params[key]}&`
+    }
+
+    return str.slice(0, -1);// removes the last ampersand
+    //return params;
+  }
+
+  /**
+   * From: http://stackoverflow.com/questions/23481979/function-to-convert-url-hash-parameters-into-object-key-value-pairs
+   * @param str
+   * @returns {{}}
+   */
+  _parseParams(str = '') {
+    var pieces = str.split("&"), data = {}, i, parts;
+    // process each query pair
+    for (i = 0; i < pieces.length; i++) {
+      parts = pieces[i].split("=");
+      if (parts.length < 2) {
+        parts.push("");
+      }
+      data[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+    }
+    return data;
+  }
+
+  /**
+   * Reads the query string from a url
+   */
+  _readQueryStringFromURL() {
+    // return this.history.location.href.split('?')[1] || '';
+    return this.history.location.search;
+  }
+
+    /**
+   * Converts a query string to a query object. Used on app load to rebuild a filter set
+   * @param str
+   */
+  _makeQueryObjectFromQueryString(str) {
+    let queryString = decodeURI(str.replace(/^\?/, '')).split("&");// Remove the first "?", then split
+    let queryParams = {};
+    let segment, value, key;
+
+    for (var i = 0; i < queryString.length; i++) {
+      segment = queryString[i].split('=');
+      key = segment[0];
+      value = segment[1] && segment[1].charAt(0) === '[' ? decodeURIComponent(segment[1]) : segment[1];
+      // Handle sort params nested object
+      if (key && key.indexOf('sort-') > -1) {
+        if (!queryParams.hasOwnProperty('sort')) {
+          queryParams.sort = {};
+        }
+
+        queryParams.sort[key.split('sort-')[1]] = value;
+
+      } else {//Filter params
+        if (key && queryParams.hasOwnProperty(key)) {
+          if (_.isArray(queryParams[key])) {
+            queryParams[key].push([value]);// Add the new value
+          } else {
+            queryParams[key] = [queryParams[key]];// Extract the string value and transform to an array
+            queryParams[key].push([value]);//Add the new value
+          }
+        } else {
+          if (key) {
+            queryParams[key] = value.split(",");// First run, add the string
+          }
+        }
+      }
+    }
+
+    return queryParams;
   }
 }
--- test/specs/apis/queries.spec.js
+++ test/specs/apis/queries.spec.js
@@ -8,9 +8,17 @@ describe('The Queries API ', () => {
     state: ["cbd0a696-2b4f-4469-85d1-f7027345e3e0"],
     genres: ["87fc3814-4cb9-43a5-b723-63ecebd65c5a", "cdc3d520-8b74-46ac-9f4c-8f27d04ab49f"],
     languages: ["9d5741e1-b482-4027-8c57-45193073ef12"],
+    entityType: ["olyplat-entity-movie"],
+    "sort-primaryGenre": "ASC",
+    // sort: { primaryGenre: 'ASC' }
+  };
+  let queryObjectLessSort = {
+    state: ["cbd0a696-2b4f-4469-85d1-f7027345e3e0"],
+    genres: ["87fc3814-4cb9-43a5-b723-63ecebd65c5a", "cdc3d520-8b74-46ac-9f4c-8f27d04ab49f"],
+    languages: ["9d5741e1-b482-4027-8c57-45193073ef12"],
     entityType: ["olyplat-entity-movie"]
   };
-  let queryString = '?state=cbd0a696-2b4f-4469-85d1-f7027345e3e0&genres=87fc3814-4cb9-43a5-b723-63ecebd65c5a,cdc3d520-8b74-46ac-9f4c-8f27d04ab49f&languages=9d5741e1-b482-4027-8c57-45193073ef12&entityType=olyplat-entity-movie';
+  let queryString = '?state=cbd0a696-2b4f-4469-85d1-f7027345e3e0&genres=87fc3814-4cb9-43a5-b723-63ecebd65c5a,cdc3d520-8b74-46ac-9f4c-8f27d04ab49f&languages=9d5741e1-b482-4027-8c57-45193073ef12&entityType=olyplat-entity-movie&sort-primaryGenre=ASC';
   let filters = [
     {"id":"state","type":"select","prop":"state","label":"State","value":["cbd0a696-2b4f-4469-85d1-f7027345e3e0"],"multiple":true,"options":{"key":"entityUUID","value":"entityValue"}},
     {"id":"genres","type":"select","prop":"genres","label":"Genres","value":["87fc3814-4cb9-43a5-b723-63ecebd65c5a","cdc3d520-8b74-46ac-9f4c-8f27d04ab49f"],"multiple":true,"multi":true,"options":{"key":"entityUUID","value":"entityValue"}},
@@ -28,15 +36,24 @@ describe('The Queries API ', () => {
   it('should have [_makeQueryObject] method', () => assert.typeOf(queriesApi._makeQueryObject, 'function'));
   it('should have [_makeQueryString] method', () => assert.typeOf(queriesApi._makeQueryString, 'function'));
   it('should have [_writeQueryStringToURL] method', () => assert.typeOf(queriesApi._writeQueryStringToURL, 'function'));
+  it('should have [_getPaginationQueryParams] method', () => assert.typeOf(queriesApi._getPaginationQueryParams, 'function'));
+  it('should have [_readQueryStringFromURL] method', () => assert.typeOf(queriesApi._readQueryStringFromURL, 'function'));
+  it('should have [_parseParams] method', () => assert.typeOf(queriesApi._parseParams, 'function'));
+  it('should have [_makeQueryObjectFromQueryString] method', () => assert.typeOf(queriesApi._makeQueryObjectFromQueryString, 'function'));
   // it('should have [getQueryObject] method', () => assert.typeOf(queriesApi.getQueryObject, 'function'));
   // it('should have [getQueryString] method', () => assert.typeOf(queriesApi.getQueryString, 'function'));
   // it('should have [getFullUrl] method', () => assert.typeOf(queriesApi.getFullUrl, 'function'));
   // it('should have [removeQueryStringFromUrl] method', () => assert.typeOf(queriesApi.removeQueryStringFromUrl, 'function'));
 
   it('_makeQueryObject should convert a filters collection to a query object', () =>
-    expect(queriesApi._makeQueryObject(filters)).to.eql(queryObject));
+    expect(queriesApi._makeQueryObject(filters)).to.eql(queryObjectLessSort));
 
   it('_makeQueryString should convert a query to a query string', () => 
     expect(queriesApi._makeQueryString(queryObject)).to.eql(queryString));
 
+  it('_writeQueryStringToURL should modify the history api by writing to the location property', () =>
+    expect(queriesApi._writeQueryStringToURL(queryString, {writeQueryStringToURL: true}).location.search).to.eql(queryString)); 
+  
+    it('_makeQueryObjectFromQueryString should convert a query object to a query string', () =>
+      expect(queriesApi._makeQueryObjectFromQueryString(queryString)).to.eql(Object.assign(queryObjectLessSort, {sort: { primaryGenre: 'ASC' }})));
 });
